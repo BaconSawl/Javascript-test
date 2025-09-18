e226e7c195efabe569a69ce282e7fdb418b92fb8
BaconSawl <ngolongvu306@gmail.com>
2025-09-18 14:38:29 +0700

    Initial repo created
    I forgot to create a repo on github whoops

---
+++ jsExercises.js
@@ -0,0 +1,26 @@
+// Add two numbers together
+
+console.log(23+97);
+
+// Add six numbers together
+console.log(11+22+33+44+55+66);
+
+// Log this. Should be 0.24675 smt
+console.log((4 + 6 + 9) / 77);
+
+// variables
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+// Try this sequence
+const max = 57;
+const actual = max - 17 ;
+const percentage = actual/max;
+
+console.log(percentage);
