83470e3657b99e108b15da7a3fafffef6c29f8dc
BaconSawl <ngolongvu306@gmail.com>
2025-09-18 14:51:53 +0700

    Messing around with console.log
    Oohhh I can add String in!

--- jsExercises.js
+++ jsExercises.js
@@ -1,26 +1,30 @@
 // Add two numbers together
 
-console.log(23+97);
+console.log("Add two numbers:", 23+97);
 
 // Add six numbers together
-console.log(11+22+33+44+55+66);
+console.log("Add six numbers:", 11+22+33+44+55+66);
 
 // Log this. Should be 0.24675 smt
-console.log((4 + 6 + 9) / 77);
+console.log("More math:", (4 + 6 + 9) / 77);
+
+console.log("----------------------------------------");
 
 // variables
+console.log("Variables test");
 let a = 10;
-console.log(a);
+console.log("Output a: ",a);
 
 a = 20;
-console.log(a);
+console.log("Re-asign a: ", a);
 
 let b = 7 * a;
-console.log(b);
+console.log("Output b = a*7: ",b);
 
+console.log("----------------------------------------");
 // Try this sequence
 const max = 57;
 const actual = max - 17 ;
 const percentage = actual/max;
 
-console.log(percentage);
+console.log("Output percentage: ", percentage);
