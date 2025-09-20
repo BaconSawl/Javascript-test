a7e795313fb07f56cae453831d0730b18a36ced6
BaconSawl <ngolongvu306@gmail.com>
2025-09-20 14:20:41 +0700

    Done with variables
    I think I understand it, i think so

--- jsExercises.js
+++ jsExercises.js
@@ -28,3 +28,35 @@ const actual = max - 17 ;
 const percentage = actual/max;
 
 console.log("Output percentage: ", percentage);
+
+// Working with variables
+
+/* 
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+
+let admin = "";
+let name = "John";
+
+admin = name ;
+window.alert(admin);
+
+/* Create a variable with the name of our planet. */
+let ourPlanetName = "Earth";  /* It’s nice to be more verbose. */
+
+/* Create a variable to store the name of a current visitor to a website. */
+let currentUserName = "John"; /* we could shorten that to userName if we know for sure that the user is current */
+
+/* Uppercase const */
+const BIRTHDAY = '18.04.1982';
+/* We generally use upper case for constants that are “hard-coded”. 
+Or, in other words, when the value is known prior to execution and directly written into the code. */
+
+const age = someCode(birthday);
+/* In contrast, age is evaluated in run-time. 
+Today we have one age, a year after we’ll have another one. 
+It is constant in a sense that it does not change through the code execution. 
+But it is a bit “less of a constant” than birthday: it is calculated, so we should keep the lower case for it. */
